2c8f6bab1405bec0ae5091900ccd8a5405756ec3
manijatin <160042249+manijatin@users.noreply.github.com>
2024-03-09 10:39:06 +0530

    Update ContactUs.js

--- my-react/src/Components/ContactUs.js
+++ my-react/src/Components/ContactUs.js
@@ -3,19 +3,46 @@ import React from 'react'
 
 function ContactUs() {
   return (
-    <div>
-      contact us
-      {/* <img src="public/EVS logo.png" alt="E-Voting System Logo" />
+   <div className="App">
 
-        <div>
-          <h1 ><u>Contact Us</u> </h1>
-          <h4> Online Voting System </h4>
-          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi vel odio veniam corporis non porro mollitia minima vero minus provident quibusdam tempore inventore, rerum exercitationem amet enim consequatur hic! Numquam?
-            Sed quis cupiditate, laudantium eius sunt fuga illum deleniti corporis eaque temporibus corrupti distinctio praesentium? Iste, officiis? Hic exercitationem repudiandae molestias sed, enim unde architecto modi laboriosam nihil non? Quam!</p>
-          <a href="home1.html"> <button className="back"> Go Back </button></a>
-        </div>
-    </div> */}
-    </div>
+     <header>
+         <div className="logo">
+           <img src="EVS logo.png" alt="E-Voting System Logo"/>
+         </div>
+         <nav>
+             <ul>
+                <li><a href="home1.html">Home</a></li>
+                <li><a href="Contact.html">Contact </a></li>
+                <li><a href="#">About Us</a></li>
+             </ul>
+        </nav>
+     </header>
+    
+    <main>
+         <div className="content">
+           <h1 ><u>Need any help ?</u> </h1>
+           <h4> Online Voting System </h4>
+           <p> <b>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi vel 
+           odio veniam corporis non porro mollitia minima 
+           vero minus provident quibusdam tempore inventore, rerum exercitationem 
+            amet enim consequatur hic! Numquam non? Quam!</b>
+          </p>
+         </div>
+         <div className="ContactUs">
+            <h4><u> Contact Us </u></h4>
+           <p> 123 Anywhere St , Any </p>
+           <p> City , ST 12345</p>
+           <p> hello@reallygreatsite.com</p>
+           <p> +123-456-7890</p>
+         </div>
+     </main>
+
+     <footer>
+        <p>@2024 Voting System. All Rights Reserved. Terms of use|Privacy Environment Policy</p>
+     </footer>
+
+  </div>
+  
   )
 }
 
