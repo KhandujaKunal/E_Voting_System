ca2946c210be4c5067ac560fbd882345f918f0f2
manijatin <160042249+manijatin@users.noreply.github.com>
2024-03-09 10:47:10 +0530

    Update About.js

--- my-react/src/Components/About.js
+++ my-react/src/Components/About.js
@@ -2,9 +2,51 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
-      About
-    </div>
+    <div className="App">
+   
+      <header> 
+              <div className="logo">
+                <img src="EVS logo.png" alt="E-Voting System Logo"/>
+              </div>
+
+          <nav>
+              <ul>
+                  <li><a href="home1.html">Home</a></li>
+                  <li><a href="Contact.html">Contact </a></li>
+                  <li><a href="#">About Us</a></li>
+              </ul>
+          </nav>
+     </header>
+     <main>
+           <div class="newimg">
+              <img src="Voting.png" alt="E-Voting System Logo"/>
+            </div> 
+
+
+         <div className="content">
+              <h1 ><u>About Us</u> </h1>
+               <h4> Online Voting System </h4>
+
+                 <p> 
+                    <b>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi vel 
+                   odio veniam corporis non porro mollitia minima 
+                   vero minus provident quibusdam tempore inventore, rerum exercitationem 
+                    amet enim consequatur hic! Numquam?</b></p>
+                  <p><b> Sed quis cupiditate, laudantium eius sunt fuga illum deleniti corporis 
+                   eaque temporibus corrupti distinctio praesentium? 
+                   Iste, officiis? Hic exercitationem repudiandae molestias sed, enim unde 
+                   architecto modi laboriosam nihil non? Quam!
+                   </b>
+                   </p>
+    
+        </div>
+        </main>
+        <footer>
+        <p>      @2024 Voting System. All Rights Reserved. Terms of use|Privacy Environment Policy</p>
+      </footer>
+
+        
+      </div>
   )
 }
 
